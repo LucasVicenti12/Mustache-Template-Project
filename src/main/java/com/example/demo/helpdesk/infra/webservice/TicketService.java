f9edeb6d571aa75d624aa0e56999003acfc145b1
Lucas Vicenti <lucasvicenti60@gmail.com>
2024-06-18 23:49:01 -0300

    first test

--- src/main/java/com/example/demo/helpdesk/infra/webservice/TicketService.java
+++ src/main/java/com/example/demo/helpdesk/infra/webservice/TicketService.java
@@ -1,17 +1,30 @@
 package com.example.demo.helpdesk.infra.webservice;
 
+import com.github.jknack.handlebars.Handlebars;
+import com.github.jknack.handlebars.Template;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/ticket")
 public class TicketService {
 
     @GetMapping
-    public String teste(Model model) {
-        model.addAttribute("nome", "Lucas");
-        return "index";
+    public String teste() {
+        return "home";
+    }
+
+    @GetMapping("/teste")
+    @ResponseBody
+    public String opa() throws Exception {
+        System.out.println("AQUI");
+        Handlebars handlebars = new Handlebars();
+
+        Template template = handlebars.compileInline("<div>Ola {{this}}</div>");
+        String teste = template.apply("Lucas");
+        System.out.println(teste);
+        return teste;
     }
 }
